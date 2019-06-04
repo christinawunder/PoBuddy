c081ca2d038f6df1df8121fe0638422de06ef277
Nikos Agathos <nicolas.agathos@gmail.com>
2019-06-04 17:01:24 +0200

    fixed pundit error

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,8 +3,7 @@ class DaysController < ApplicationController
   skip_before_action :authenticate_user!, only: :about
 
   def index
-    @days = Day.all
-    authorize @day
+    @days = policy_scope(Day)
   end
 
   def new
