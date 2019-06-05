01ad78a80719a7a9521839181a533295db4f7dcd
Christina Wunder <wunder.christina@icloud.com>
2019-06-05 09:12:47 +0200

    started working on create_advice

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -48,4 +48,46 @@ class DaysController < ApplicationController
   def day_params
     params.require(:day).permit(:bleeding, :bad_mood, :head_pain, :abdominal_pain, :breast_pain, :date, :user_id)
   end
+
+  def create_advice
+    @advice = Advice.new(day_symptoms)
+  end
+
+  def day_symptoms
+    if @day.bleeding >= 2 && @day.bleeding < 4
+      # take 1 bleeding article
+    elsif @day.bleeding >= 4
+      # take 2 bleeding articles
+    end
+
+    if @day.bad_mood >= 2 && @day.bad_mood < 4
+      # take 1 bad_mood article
+    elsif @day.bad_mood >= 4
+      # take 2 bad_mood articles
+    end
+
+    if @day.head_pain >= 2 && @day.head_pain < 4
+      # take 1 head_pain article
+    elsif @day.head_pain >= 4
+      # take 2 head_pain articles
+    end
+
+    if @day.abdominal_pain >= 2 && @day.abdominal_pain < 4
+      # take 1 abdominal_pain article
+    elsif @day.abdominal_pain >= 4
+      # take 2 abdominal_pain articles
+    end
+
+    if @day.breast_pain >= 2 && @day.breast_pain < 4
+      # take 1 breast_pain article
+    elsif @day.breast_pain >= 4
+      # take 2 breast_pain articles
+    end
+  end
 end
+
+
+
+
+
+
