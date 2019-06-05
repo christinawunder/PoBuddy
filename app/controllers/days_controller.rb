878091b441978a0ae1c467dc385d2a8ea1428dc1
Christina Wunder <wunder.christina@icloud.com>
2019-06-05 12:04:59 +0200

    created a create-advice method, but not tested yet

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -14,7 +14,7 @@ class DaysController < ApplicationController
   def create
     @day = Day.new(day_params)
     @day.user = current_user
-    # create the day.links (trhough advices)
+    create_advice_combo # private method below
     authorise @day
     @day.save!
   end
@@ -49,45 +49,52 @@ class DaysController < ApplicationController
     params.require(:day).permit(:bleeding, :bad_mood, :head_pain, :abdominal_pain, :breast_pain, :date, :user_id)
   end
 
-  def create_advice
-    @advice = Advice.new(day_symptoms)
+  def daily_advices
+    advices_combo = []
+    advices_combo << [bleeding_links, bad_mood_links, head_pain_links, abdominal_pain_links, breast_pain_links]
   end
 
-  def day_symptoms
-    if @day.bleeding >= 2 && @day.bleeding < 4
-      # take 1 bleeding article
+  def create_advice
+    @advice = Advice.new(symptom)
+  end
+
+  def bleeding_links
+    if @day.bleeding >= 1 && @day.bleeding < 4
+      create_advice(Link.all.where(category: 'bleeding').sample)
     elsif @day.bleeding >= 4
-      # take 2 bleeding articles
+      2.times(create_advice(Link.all.where(category: 'bleeding').sample))
     end
+  end
 
-    if @day.bad_mood >= 2 && @day.bad_mood < 4
-      # take 1 bad_mood article
+  def bad_mood_links
+    if @day.bad_mood >= 1 && @day.bad_mood < 4
+      create_advice(Link.all.where(category: 'bad mood').sample)
     elsif @day.bad_mood >= 4
-      # take 2 bad_mood articles
+      2.times(create_advice(Link.all.where(category: 'bad mood').sample))
     end
+  end
 
-    if @day.head_pain >= 2 && @day.head_pain < 4
-      # take 1 head_pain article
+  def head_pain_links
+    if @day.head_pain >= 1 && @day.head_pain < 4
+      create_advice(Link.all.where(category: 'head_pain').sample)
     elsif @day.head_pain >= 4
-      # take 2 head_pain articles
+      2.times(create_advice(Link.all.where(category: 'head_pain').sample))
     end
+  end
 
+  def abdominal_pain_links
     if @day.abdominal_pain >= 2 && @day.abdominal_pain < 4
-      # take 1 abdominal_pain article
+      create_advice(Link.all.where(category: 'abdominal pain').sample)
     elsif @day.abdominal_pain >= 4
-      # take 2 abdominal_pain articles
+      2.times(create_advice(Link.all.where(category: 'abdominal pain').sample))
     end
+  end
 
+  def breast_pain_links
     if @day.breast_pain >= 2 && @day.breast_pain < 4
-      # take 1 breast_pain article
+      create_advice(Link.all.where(category: 'breast pain').sample)
     elsif @day.breast_pain >= 4
-      # take 2 breast_pain articles
+      2.times(create_advice(Link.all.where(category: 'breast pain').sample))
     end
   end
 end
-
-
-
-
-
-
