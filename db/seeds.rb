a14db24e6d590b2e093b41061f743c6fa2f9aeb5
Nikos Agathos <nicolas.agathos@gmail.com>
2019-06-06 16:31:57 +0200

    nikolaki changed seed a little bit...

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 User.create(email: "test@test.com", password: "123456789")
 
 Day.destroy_all
-Day.create!(date: Date.today, bleeding: 1, bad_mood: 3, head_pain: 2, abdominal_pain: 2, breast_pain: 1, user_id: 1)
 Day.create!(date: Date.today - 1, bleeding: 3, bad_mood: 3, head_pain: 4, abdominal_pain: 4, breast_pain: 2, user_id: 1)
 Day.create!(date: Date.today - 2, bleeding: 4, bad_mood: 1, head_pain: 3, abdominal_pain: 1, breast_pain: 3, user_id: 1)
+Day.create!(date: Date.today - 3, bleeding: 5, bad_mood: 3, head_pain: 2, abdominal_pain: 2, breast_pain: 1, user_id: 1)
 
 
 Link.destroy_all
