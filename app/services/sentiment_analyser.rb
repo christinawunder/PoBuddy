85cb9fcd4c8a3c859a7801c4330b8edf030a9d7b
Christina Wunder <wunder.christina@icloud.com>
2019-06-07 13:14:42 +0200

    Alexis API magic

---
+++ app/services/sentiment_analyser.rb
@@ -0,0 +1,66 @@
+require "ibm_watson"
+
+class SentimentAnalyser
+
+  def initializer
+    set_client
+  end
+
+  def call(text)
+    @text = text
+    perform_request
+    evaluate_result
+    expose_result
+  end
+
+  private
+
+  attr_reader :client, :result, :text
+
+  def perform_request
+    @result = {
+      "document_tone": {
+        "tones": [
+          {
+            "score": 0.6165,
+            "tone_id": "sadness",
+            "tone_name": "Sadness"
+          },
+          {
+            "score": 0.829888,
+            "tone_id": "analytical",
+            "tone_name": "Analytical"
+          }
+        ]
+      }
+    }
+    # @result = client.tone(
+    #   tone_input: { text: text },
+    #   content_type: "application/json"
+    # )
+  end
+
+  def evaluate_result
+
+  end
+
+  def expose_result
+    result
+  end
+
+  def set_client
+    @client = IBMWatson::ToneAnalyzerV3.new(
+      version: "2017-09-21",
+      iam_apikey: api_key,
+      url: url
+    )
+  end
+
+  def api_key
+    # COMES FROM YOUR SECRETS (ON Monday)
+  end
+
+  def url
+    # COMES ON Monday
+  end
+end
