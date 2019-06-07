70400efb25af80f639856465d63b34332a5052c2
Nikos Agathos <nicolas.agathos@gmail.com>
2019-06-07 12:42:23 +0200

    added symbol for pain and mood

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,4 +2,8 @@ class Day < ApplicationRecord
   belongs_to :user
   has_many :advices
   has_many :links, through: :advices
+
+  def pain?
+    head_pain || abdominal_pain || breast_pain
+  end
 end
