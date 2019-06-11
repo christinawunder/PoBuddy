8b41911fb95e7930a5dfbab2d4f51c006b171d76
justine moreau <juhmoreau@gmail.com>
2019-06-11 18:28:06 +0200

    added the read more btn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,4 @@ import {sliderValue} from "../components/slider";
 import {loadingAnimation} from "../components/loading_mascot.js"
 sliderValue();
 loadingAnimation();
+import {readMore} from "..components/read_more.js"
