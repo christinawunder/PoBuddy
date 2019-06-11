9c4c51c95fd7da645bebd748c24fb4d519d896c8
Nikos Agathos <nicolas.agathos@gmail.com>
2019-06-11 13:51:34 +0200

    date validation error custom, date loophole counter-measure enabled

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,8 +2,12 @@ class Day < ApplicationRecord
   belongs_to :user
   has_many :advices, dependent: :destroy
   has_many :links, through: :advices
-  validates :date, uniqueness: true
+  validates :date, :uniqueness => { :message => " seems like it has already been taken. Pick another day, or click on this day, delete its data and enter new infos" }
   serialize :emotion
+  # validates :head_pain, presence: true
+  # validates :abdominal_pain, presence: true
+  # validates :breast_pain, presence: true
+  # validates :bad_mood, presence: true
 
   def pain?
     head_pain || abdominal_pain || breast_pain
