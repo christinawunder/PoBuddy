c1d6fe627a52802f9c9140a4b847cefef34a5c11
justine moreau <juhmoreau@gmail.com>
2019-06-12 12:15:22 +0200

    added loading gif

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import {sliderValue} from "../components/slider";
 import {loadingAnimation} from "../components/loading_mascot.js"
-import {readMore} from "..components/read_more.js"
 sliderValue();
 loadingAnimation();
-readMore();
+
